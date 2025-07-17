1d3881c8df060b1c48820c48cf036eb02007a9df
HLockward <HLockward12@gmail.com>
2025-07-16 23:28:44 -0400

    Developed the feature of GetProductByCategory

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,7 +1,6 @@
 namespace Catalog.API.Products.GetProductById;
 
 public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
-
 public record GetProductByIdResult(Product Product);
 
 internal class GetProductByIdQueryHandler
