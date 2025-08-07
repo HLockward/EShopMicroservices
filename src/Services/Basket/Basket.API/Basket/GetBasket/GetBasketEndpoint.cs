289e143dab361c8237af1352d537ec8c3622e7b8
HLockward <HLockward12@gmail.com>
2025-08-07 13:00:28 -0400

    Developed the GetBasket feature with CQRS and MediatR, minimal API and Carter

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,6 +1,21 @@
 namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketEndpoint
-{
+//public record GetBasketRequest(string UserName);
+public record GetBasketResponse(ShoppingCart Cart);
 
+public class GetBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(userName));
+            var response = result.Adapt<GetBasketResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetBasket")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .WithSummary("Get Basket by user name")
+        .WithDescription("Get Basket by user name");
+    }
 }
