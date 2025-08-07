33ced310855c0821bb834d433b82f1d954f83869
HLockward <HLockward12@gmail.com>
2025-08-07 13:20:07 -0400

    Developed the StoreBasket feature with CQRS and MediatR, minimal API and Carter

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -1,6 +1,23 @@
 namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketEndpoint
+public record StoreBasketRequest(ShoppingCart Cart);
+public record StoreBasketResponse(string UserName);
+public class StoreBasketEndpoint : ICarterModule
 {
-
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<StoreBasketResponse>(); 
+            return Results.Created($"/basket/{response.UserName}", response);
+        })
+        .WithName("StoreBasket")
+        .WithSummary("Stores a shopping cart for a user.")
+        .Produces<StoreBasketResponse>()
+        .ProducesValidationProblem()
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithTags("Basket");
+    }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,23 @@
 namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketHandler
-{
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
 
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(command => command.Cart).NotNull().WithMessage("Shopping cart cannot be null.");
+        RuleFor(command => command.Cart.UserName).NotEmpty().WithMessage("UserName is required.");
+    }    
+}
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+        // Here you would typically call a service to store the basket in a database or cache.
+        return new StoreBasketResult("swd");
+    }
 }
