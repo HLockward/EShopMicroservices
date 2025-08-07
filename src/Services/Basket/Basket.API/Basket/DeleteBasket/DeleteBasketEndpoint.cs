a3206763e06bb46bcebcbb8a551d0646c086c8d3
HLockward <HLockward12@gmail.com>
2025-08-07 14:08:28 -0400

    Developed the DeleteBasket feature with CQRS and MediatR, minimal API and Carter

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,21 @@
 namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketEndpoint
+//public record DeleteBasketRequest(string UserName);
+public record DeleteBasketResponse(bool IsSuccess);
+public class DeleteBasketEndpoint : ICarterModule
 {
-
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(userName));
+            var response = result.Adapt<DeleteBasketResponse>();
+            return Results.Ok(response);
+        }).WithName("DeleteBasket")
+          .WithSummary("Delete a user's basket")
+          .WithDescription("Deletes the basket for the specified user.")
+          .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status400BadRequest)
+          .ProducesProblem(StatusCodes.Status404NotFound);
+    }
 }
